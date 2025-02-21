11d79aba881ecad8249abef0419b187595a81141
SachaG3 <sacha.guignard@sts-sio-caen.info>
2025-02-21 09:31:56 +0100

    +fix

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -3,14 +3,26 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Personne;
+use App\Models\Utilisateur;
 
 class DashboardTest extends TestCase
 {
     use RefreshDatabase;
+
     public function testDashboardAccessible()
     {
+        $personne = Personne::factory()->create([
+            'courriel' => 'test@example.com'
+        ]);
+
+        $user = Utilisateur::factory()->create([
+            'id' => $personne->id,
+        ]);
+
+        $this->actingAs($user);
+
         $response = $this->get('/dashboard');
 
         $response->assertStatus(200);
